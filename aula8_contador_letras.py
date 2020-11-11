3b78f15ef8e16dd11eb067a9988b79adaa587266
Fernando Lopes <fnando0506@gmail.com>
2020-11-10 21:29:57 -0300

    lambda

--- aula8_contador_letras.py
+++ aula8_contador_letras.py
@@ -5,6 +5,9 @@ def contador_letras(lista_palavras):
         contador.append(quantidade)
     return contador
 
+def teste():
+    return 'teste'
+
 if __name__ == '__main__':
     lista = ['cachorro', 'gato']
     print(contador_letras(lista))
--- aula8_importacao.py
+++ aula8_importacao.py
@@ -1,6 +1,6 @@
 from aula7_televisao import Televisao
 from aula7_calculadora1 import Calculadora
-from aula8_contador_letras import contador_letras
+from aula8_contador_letras import contador_letras, teste
 
 if __name__ == '__main__':
     televisao = Televisao()
@@ -10,4 +10,6 @@ if __name__ == '__main__':
     calculadora = Calculadora(5, 10)
     print(calculadora.soma())
     lista = ['cachorro', 'gato', 'elefante']
-    print(contador_letras(lista))
+    total_letras = contador_letras(lista)
+    print('Total de letras por palavra de lista: {}' .format(total_letras))
+    print(teste())
